d09afe80beeaacb671a0a10c7df910efab910df7
J3ffn <jefferson.mangueira12@gmail.com>
2023-06-28 00:53:02 -0300

    rm

--- src/Model/Personagem.java
+++ src/Model/Personagem.java
@@ -11,16 +11,6 @@ public class Personagem {
     private EstadoPersonagem estadoAtual;
     private BufferedImage imagem;
 
-    public Personagem() {
-        // Estado inicial: Andando
-        try {
-            imagem = ImageIO.read(new File("./img/background/Leowood.png"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        estadoAtual = new EstadoAndando();
-    }
-
     public void setEstado(EstadoPersonagem estado) {
         this.estadoAtual = estado;
     }

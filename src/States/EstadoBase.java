563a5fdc5ceb8c878a35a9313c0c01d0b26f8e21
Santiago <souto.santiago@academico.ifpb.edu.br>
2023-06-27 21:28:48 -0300

    atualização do estadoBase

--- src/States/EstadoBase.java
+++ src/States/EstadoBase.java
@@ -7,4 +7,11 @@ public class EstadoBase extends EstadoPersonagem{
     private int defesa = 0;
     private int ataque = 2;
     private int vidaMaxima = 10;
+    
+    public EstadoBase() {
+    	velocidade = 1;
+    	defesa = 0;
+    	ataque = 2;
+    	vidaMaxima = 10;
+	}
 }

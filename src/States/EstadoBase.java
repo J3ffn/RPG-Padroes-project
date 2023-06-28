735247156a574361933f858c71ecd6407fc08597
Santiago <souto.santiago@academico.ifpb.edu.br>
2023-06-27 21:35:57 -0300

    main atualizado

--- src/States/EstadoBase.java
+++ src/States/EstadoBase.java
@@ -3,15 +3,11 @@ package States;
 import Model.Personagem;
 
 public class EstadoBase extends EstadoPersonagem{
-	private float velocidade = 1;
-    private int defesa = 0;
-    private int ataque = 2;
-    private int vidaMaxima = 10;
     
     public EstadoBase() {
-    	velocidade = 1;
-    	defesa = 0;
-    	ataque = 2;
-    	vidaMaxima = 10;
+    	setVelocidade(1);
+    	setDefesa(0);
+    	setAtaque(2);
+    	setVidaMaxima(10);
 	}
 }
---
+++ src/States/EstadoFuria.java
@@ -0,0 +1,10 @@
+package States;
+
+public class EstadoFuria extends EstadoPersonagem{
+	public EstadoFuria() {
+    	setVelocidade(2);
+    	setDefesa(0);
+    	setAtaque(50);
+    	setVidaMaxima(2);
+	}
+}

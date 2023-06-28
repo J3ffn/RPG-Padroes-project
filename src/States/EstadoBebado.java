b0b2e4a31e623fdad5b824f36557d51283655331
Eduardo9345 <jose.evangelista@academico.ifpb.edu.br>
2023-06-27 22:37:24 -0300

    Adição Estado Bêbado

---
+++ src/States/EstadoBebado.java
@@ -0,0 +1,8 @@
+package States;
+
+public class EstadoBebado extends EstadoPersonagem{
+	private float velocidade = 0.5f;
+    private int defesa = 0;
+    private int ataque = 2;
+    private int vidaMaxima = 10;
+}

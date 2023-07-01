88c548657a20d45c7752b479350cbb3dd02c3cd1
Eduardo9345 <jose.evangelista@academico.ifpb.edu.br>
2023-07-01 20:28:04 -0300

    Update Zombie

--- src/Model/Zombie.java
+++ src/Model/Zombie.java
@@ -4,6 +4,7 @@ import States.EstadoBase;
 
 public class Zombie extends Personagem{
 	public Zombie() {
+		this.setVida(10);
 		this.setEstadoAtual(new EstadoBase());
 	}
 }
--- src/View/ZombieView.java
+++ src/View/ZombieView.java
@@ -2,29 +2,43 @@ package View;
 
 import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class ZombieView extends Component {
+import Model.Personagem;
+import Model.Zombie;
 
+public class ZombieView extends Component {
+	
 	  private String caminhoSprite;
+	  private Zombie zumbi;	  
 	  
 	  public ZombieView(String caminho, int x, int y) {
-		  this.setBounds(x, y, 15, 27);
+		  this.setBounds(x, y, 15, 50);
 		  caminhoSprite = caminho;
+		  zumbi = null;
 	  }
+	  
 	  public ZombieView(int x, int y) {
-		  this.setBounds(x, y, 15, 27);
-		  caminhoSprite = "img/sprites/Zombie/Idle_Zombie_frente.png";
+		  this("img/sprites/Zombie/Idle_Zombie_frente.png", x, y);
 	  }
+	  
+	  public ZombieView(Zombie zombie, String caminho, int x, int y) {
+		  this(caminho, x, y);
+		  zumbi = zombie;
+	  }
+	  
 	  public void setSprite(String path) {
 	    caminhoSprite = path;
 	  }
-
+	  
 
 	  @Override
 	  public void paint(Graphics g) {
@@ -35,7 +49,17 @@ public class ZombieView extends Component {
 	    } catch (IOException e) {
 	      throw new RuntimeException(e);
 	    }
-
 	    g.drawImage(image, 0, 0, 15, 27, null);
+	  }
+	  
+	  public boolean atacar(Personagem inimigo) {
+		    return (zumbi != null)? zumbi.atacar(inimigo): false;
+	  }
+	  public boolean perderVida(int dano) {
+		  return (zumbi != null)? zumbi.perderVida(dano): false;
+	  }
+	  
+	  public boolean isVivo() {
+		    return (zumbi != null)? zumbi.isVivo(): false;
 	  }
 }

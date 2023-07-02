9e45b1be8e984a8458486fdfa95ef76c807c0118
J3ffn <jefferson.mangueira12@gmail.com>
2023-07-02 14:24:16 -0300

    add Gerenciador global de teclado

--- src/Model/GerenciadorPocoes.java
+++ src/Model/GerenciadorPocoes.java
@@ -10,20 +10,24 @@ public class GerenciadorPocoes extends GerenciadorElementos {
   public void CriarElementos() {
     Pocao hidromel = new Pocao(152, 173, "img/pocoes/hidromel.png");
     hidromel.setNome("Hidromel");
+    hidromel.setName("Poção de Hidromel");
     hidromel.setEstado(new EstadoBebado());
     elementos.add(hidromel);
 
     Pocao pocaoVelocidade = new Pocao(216, 370, "img/pocoes/cura.png");
     pocaoVelocidade.setNome("Poçao de velocidade");
+    pocaoVelocidade.setName("Poção de Velocidade");
     pocaoVelocidade.setEstado(new EstadoFlash());
     elementos.add(pocaoVelocidade);
 
     Pocao pocaoVida = new Pocao(323, 152, "img/pocoes/velocidade.png");
     pocaoVida.setNome("Poçao de vida");
+    pocaoVida.setName("Poção de Vida");
     elementos.add(pocaoVida);
 
     Pocao copoLeite = new Pocao(153, 173, "img/pocoes/furia.png");
     copoLeite.setNome("Copo de leite");
+    pocaoVelocidade.setName("Poção de Leite");
     copoLeite.setEstado(new EstadoBase());
     elementos.add(copoLeite);
   }
--- src/View/GerenciadorTeclado.java
+++ src/View/GerenciadorTeclado.java
@@ -3,21 +3,32 @@ package View;
 import java.awt.Component;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import Model.Pocao;
 import Ouvintes.OuvintePassosPersonagem;
 
 public class GerenciadorTeclado implements KeyListener {
 
   private PersonagemView personagem;
   private OuvintePassosPersonagem ouvintePersonagem;
-  List<Component> components;
+  List<Pocao> pocoes;
 
   public GerenciadorTeclado(PersonagemView p, Component[] components) {
     personagem = p;
-    this.components = Arrays.asList(components);
     ouvintePersonagem = new OuvintePassosPersonagem(p);
+    addPocoes(components);
+  }
+
+  private void addPocoes(Component[] components) {
+    pocoes = new ArrayList<>();
+    for (Component c : components) {
+      if (c.getName() != null && c.getName().startsWith("Poção")) {
+        pocoes.add((Pocao) c);
+      }
+    }
   }
 
   @Override
@@ -31,9 +42,11 @@ public class GerenciadorTeclado implements KeyListener {
 
     Integer x = personagem.getSpriteX();
     Integer y = personagem.getSpriteY();
-    components.forEach(p -> {
-      Integer tamP = p.getWidth();
-      if (x >= p.getX() && x <= p.getX() + tamP) {  // TODO Refazer alogica dessa condição e adicionar o efeito de cada estado no personagem em cima da poção
+    pocoes.forEach(p -> {
+      Integer xEsq = p.getxCantoEsquerdo();
+      Integer xDir = p.getxCantoDireito();
+      // if () { // TODO Refazer alogica dessa condição e adicionar o efeito de cada
+                                                   // estado no personagem em cima da poção
         System.out.println("Está no meio");
 
       }
